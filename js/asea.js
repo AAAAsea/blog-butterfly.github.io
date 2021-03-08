2aedf9b46bc52974291bb8e8aea77f26ea3b6478
AAAAsea <569389750@qq.com>
2021-03-08 22:25:22 +0800

    Blog

--- js/asea.js
+++ js/asea.js
@@ -71,3 +71,18 @@ hexo.extend.helper.register('getAnimalIcon', function (year) {
     }
     return icon[index]
 });
+
+// tianqi
+
+(function(a,h,g,f,e,d,c,b){b=function(){d=h.createElement(g);c=h.getElementsByTagName(g)[0];d.src=e;d.charset="utf-8";d.async=1;c.parentNode.insertBefore(d,c)};a["SeniverseWeatherWidgetObject"]=f;a[f]||(a[f]=function(){(a[f].q=a[f].q||[]).push(arguments)});a[f].l=+new Date();if(a.attachEvent){a.attachEvent("onload",b)}else{a.addEventListener("load",b,false)}}(window,document,"script","SeniverseWeatherWidget","//cdn.sencdn.com/widget2/static/js/bundle.js?t="+parseInt((new Date().getTime() / 100000000).toString(),10)));
+window.SeniverseWeatherWidget('show', {
+flavor: "bubble",
+location: "WX4FBXXFKE4F",
+geolocation: true,
+language: "zh-Hans",
+unit: "c",
+theme: "auto",
+token: "76988148-e959-4715-9950-b539b593162a",
+hover: "enabled",
+container: "tp-weather-widget"
+})
